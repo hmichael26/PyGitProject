fb494dcbba25f46fb2a181a25a20a55b30455e9f
miko <hmichael26@gmail.com>
2023-03-22 21:50:12 +0200

    last commit

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hiiiii(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hiiiii('PyCharm')
+    print_hiiiii('PyCharm is great!')
     
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
